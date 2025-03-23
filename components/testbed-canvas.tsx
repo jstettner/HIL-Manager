ce79d62cb297266ecc65c418700ec397fe05e699
Jack Stettner <jack@stet.com>
2025-03-22 18:27:37 -0700

    pretty decent looking canvas

--- components/testbed-canvas.tsx
+++ components/testbed-canvas.tsx
@@ -1,55 +1,55 @@
 "use client";
-import React, { useCallback } from 'react';
+import React, { useCallback } from "react";
 
 import {
-    ReactFlow,
-    MiniMap,
-    Background,
-    BackgroundVariant,
-    useNodesState,
-    useEdgesState,
-    addEdge,
-    Connection,
-    Edge
-  } from '@xyflow/react';
+  ReactFlow,
+  MiniMap,
+  Background,
+  BackgroundVariant,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  Connection,
+  Edge,
+} from "@xyflow/react";
 
 import "@xyflow/react/dist/style.css";
 
 const initialNodes = [
   {
-    id: '1',
-    type: 'input',
-    data: { label: 'Input Node' },
+    id: "1",
+    type: "input",
+    data: { label: "Input Node" },
     position: { x: 250, y: 25 },
   },
- 
+
   {
-    id: '2',
+    id: "2",
     // you can also pass a React component as a label
     data: { label: <div>Default Node</div> },
     position: { x: 100, y: 125 },
   },
   {
-    id: '3',
-    type: 'output',
-    data: { label: 'Output Node' },
+    id: "3",
+    type: "output",
+    data: { label: "Output Node" },
     position: { x: 250, y: 250 },
   },
 ];
- 
+
 const initialEdges = [
-  { id: 'e1-2', source: '1', target: '2' },
-  { id: 'e2-3', source: '2', target: '3', animated: true },
+  { id: "e1-2", source: "1", target: "2" },
+  { id: "e2-3", source: "2", target: "3", animated: true },
 ];
 
 export function TestbedCanvas() {
-const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-const onConnect = useCallback(
+  const onConnect = useCallback(
     (params: Connection | Edge) => setEdges((eds) => addEdge(params, eds)),
     [setEdges],
-);
+  );
   return (
     <div className="w-full h-full">
       <ReactFlow
