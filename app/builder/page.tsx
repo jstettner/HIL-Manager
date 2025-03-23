c11511df70f7e2a700d1163a3ac91947005ce65d
Jack Stettner <jack@stet.com>
2025-03-22 17:49:23 -0700

    reactflow styling

--- app/builder/page.tsx
+++ app/builder/page.tsx
@@ -3,14 +3,15 @@ import { TestbedCanvas } from "@/components/testbed-canvas";
 
 export default function TestbedBuilderPage() {
   return (
-    <div className="flex flex-col h-[calc(100vh-4rem)] p-6">
+    <div className="flex flex-col h-[calc(100vh-5rem)] p-6">
       <div className="flex items-center gap-2 mb-6 page-header">
         <SquareDashedMousePointer className="w-6 h-6" />
         <div className="flex flex-row items-baseline gap-2">
           <div className="flex flex-row items-baseline">
             {/* Hack to fix the kerning between the words. */}
-            <h1 className="text-2xl pr-0 mr-0">Testbed Bui</h1>
-            <h1 className="text-2xl pl-0 ml-[-0.17em]">lder</h1>
+            <h1 className="text-2xl pr-3">
+              Testbed Bui<span className="ml-[-0.17em]">l</span>der
+            </h1>
           </div>
           <h3 className="text-xl text-muted-foreground">
             Add a Test Environment to the Armory
--- components/testbed-canvas.tsx
+++ components/testbed-canvas.tsx
@@ -1,24 +1,48 @@
 "use client";
-import React from 'react';
-import { ReactFlow } from '@xyflow/react';
- 
-import '@xyflow/react/dist/style.css';
- 
+import React, { useCallback } from 'react';
+
+import {
+    ReactFlow,
+    MiniMap,
+    Background,
+    BackgroundVariant,
+    useNodesState,
+    useEdgesState,
+    addEdge,
+    Connection,
+    Edge
+  } from '@xyflow/react';
+
+import "@xyflow/react/dist/style.css";
+
 const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
 ];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
- 
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
 export function TestbedCanvas() {
+const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+const onConnect = useCallback(
+    (params: Connection | Edge) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges],
+);
   return (
     <div className="w-full h-full">
-        <ReactFlow 
-          nodes={initialNodes} 
-          edges={initialEdges}
-          fitView
-          className="h-full"
-        />
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        className="flow-canvas"
+      >
+        {/* <Controls /> */}
+        <MiniMap />
+        <Background variant={BackgroundVariant.Dots} gap={24} size={1} />
+      </ReactFlow>
     </div>
   );
 }
