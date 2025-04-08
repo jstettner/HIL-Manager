37656c967fcdc0a57fcea8596d7624e48b1dddf0
Sam Brickman <sambrickman@Sams-MacBook-Pro-2.local>
2025-04-08 19:54:37 -0400

    Copy changes for the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -45,10 +45,9 @@ export default function Home() {
       >
         <div className="h-full flex flex-col justify-around items-center gap-12 px-15 sm:px-20">
           <div className="flex flex-col max-w-2xl items-center text-center mt-[30vh] sm:mt-[40vh]">
-            <h1 className="hero-text">Fortify your Product</h1>
+            <h1 className="hero-text">Catch Critical Failures<br/> Before they Happen.</h1>
             <h2 className="hero-subtext">
-              Protect against costly real-world failures with AI-Agents for
-              Hardware Verification.
+              AI agents that connect to your hardware and test like your most experienced engineer - reducing days of testing to seconds.
             </h2>
             <Link
               href={
@@ -70,38 +69,35 @@ export default function Home() {
       <div className="min-h-80 flex flex-1 flex-col gap-5 w-full px-15 sm:px-40 justify-center items-center">
         <div className="h-full mt-20">
           <div className="text-center max-w-3xl">
-            <h2 className="text-4xl">HIL and SIL</h2>
+            <h2 className="text-4xl"> One platform. Any test environment. </h2>
             <p className="text-gray-300 text-lg">
-              Our framework is designed for both hardware-in-the-loop (HIL) and
-              software-in-the-loop (SIL) environments–reducing the friction of
-              setting up and running tests. Tests are written to execute
-              remotely and intelligently in all environments.
+            Whether your team tests with physical hardware, simulations, or digital twins, Halidom helps you ship faster and catch issues earlier. We integrate diretly with your hardware-in-the-loop (HIL) and software-in-the-loop (SIL) environments, enabling frictionless test setup and execution for even the most complex systems.
             </p>
           </div>
         </div>
         <div className="w-1/2 border-b border-gray-500 my-20" />
         <div className="h-full mb-30 grid auto-rows-min gap-30 lg:gap-7 lg:grid-cols-3">
           <div className="text-center max-w-sm">
-            <h2 className="text-4xl mb-2">Adaptive</h2>
+            <h2 className="text-4xl mb-2">Automated <br/> Test Planning</h2>
             <p className="text-gray-300 text-lg">
-              We analyze your system requirements, design, and implementation to
-              create comprehensive test plans on a change-by-change basis.
+          Our AI agents generate tailored test plans for every code change, revealing issues even your most cautious engineers might miss.
             </p>
           </div>
 
           <div className="text-center max-w-sm">
-            <h2 className="text-4xl mb-2">Thorough</h2>
+            <h2 className="text-4xl mb-2">Comprehensive Validation</h2>
             <p className="text-gray-300 text-lg">
-              We perform both regression testing and new feature validation to
-              ensure your system is working as expected.
+            From new features to regressions, Halidom verifies that your system behaves exactly as expected, every time.
+
             </p>
           </div>
 
           <div className="text-center max-w-sm">
-            <h2 className="text-4xl mb-2">Insightful</h2>
+            <h2 className="text-4xl mb-2"> Compliance-Ready Reporting </h2>
             <p className="text-gray-300 text-lg">
-              We provide data-driven insights to help you understand the impact
-              of your changes and generate verification reports for compliance.
+           Get instant insights into how changes impact your system - with auto-generated reports for audits, certifications, and safety standards. 
+
+
             </p>
           </div>
         </div>
@@ -121,15 +117,13 @@ export default function Home() {
       <div className="min-h-80 flex flex-1 flex-col gap-5 w-full px-15 sm:px-40 justify-center items-center">
         <div className="h-full mt-20">
           <div className="text-center max-w-xl">
-            <h2 className="text-4xl">AI Native</h2>
+            <h2 className="text-4xl">AI native, built for the physical world.</h2>
             <p className="text-gray-300 text-lg">
-              Halidom is built from the ground up around bleeding edge AI
-              techniques. Our entire stack, from change analysis to test
-              execution to data review, is powered by generative AI.
+              Halidom enables your physical systems to interface seamlessly with today's most advanced AI models, helping you ship faster, smarter and safer. We bridge the gap between bleeding-edge AI in the digital world with the messy challenges of shipping in the physical world.
               <br />
               <br />
-              We deliver a level of flexibility that has never been possible
-              with traditional testing tools.
+              Our world-class engineering team has built and validated some of the world’s most complex physical systems — and now we’re building the platform we always wished we had.
+
             </p>
           </div>
         </div>
